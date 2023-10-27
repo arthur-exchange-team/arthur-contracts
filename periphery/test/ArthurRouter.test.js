c1f49ec80bc3cfa443a77813a3e43b179901d1a1
EricNguyenFly <huynguyen.flyteam@gmail.com>
2023-10-27 10:29:19 +0700

    add test lock time and start time

--- periphery/test/ArthurRouter.test.js
+++ periphery/test/ArthurRouter.test.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const contractJson = require('excalibur-core/artifacts/contracts/ArthurFactory.sol/ArthurFactory.json');
-const { getCurrentTimestamp } = require('./utils');
+const ArthurPairJson = require('excalibur-core/artifacts/contracts/ArthurPair.sol/ArthurPair.json');
+const { getCurrentTimestamp, skipTime } = require('./utils');
 
 ONE_DAY = 1 * 24 * 60 * 60;
 ONE_ETHER = ethers.utils.parseEther('1');
@@ -21,6 +22,7 @@ describe('ArthurRouter', () => {
         const ERC20 = await ethers.getContractFactory('ERC20');
 
         const ArthurFactory = new ethers.ContractFactory(contractJson.abi, contractJson.bytecode, owner);
+        ArthurPair = new ethers.ContractFactory(ArthurPairJson.abi, ArthurPairJson.bytecode, owner);
         const WETH9 = await ethers.getContractFactory('WETH9');
         const ArthurRouter = await ethers.getContractFactory('ArthurRouter');
 
@@ -48,6 +50,34 @@ describe('ArthurRouter', () => {
             const currentTimestamp = await getCurrentTimestamp();
             await arthurFactory.createPair(tokenA.address, tokenB.address, ONE_DAY, currentTimestamp + ONE_DAY);
             await arthurRouter.addLiquidity(tokenA.address, tokenB.address, ETHER_100K, ETHER_100K.div(2), ETHER_100K, ETHER_100K.div(2), owner.address, currentTimestamp + ONE_DAY, ONE_DAY, currentTimestamp + ONE_DAY);
+        });
+
+        it('check lock time', async () => {
+            let currentTimestamp = await getCurrentTimestamp();
+            await arthurFactory.createPair(tokenA.address, tokenB.address, ONE_DAY, currentTimestamp + ONE_DAY);
+            await arthurRouter.addLiquidity(tokenA.address, tokenB.address, ETHER_100K, ETHER_100K.div(2), ETHER_100K, ETHER_100K.div(2), owner.address, currentTimestamp + ONE_DAY, ONE_DAY, currentTimestamp + ONE_DAY);
+            await expect(arthurRouter.removeLiquidity(tokenA.address, tokenB.address, 1, 0, 0, owner.address, currentTimestamp + ONE_DAY)).to.be.revertedWith("INVALID_TIME_LOCK");
+
+            await skipTime(ONE_DAY + 1);
+            currentTimestamp = await getCurrentTimestamp();
+
+            const pair = await arthurRouter.getPair(tokenA.address, tokenB.address);
+            const arthurPair = await ArthurPair.attach(pair);
+
+            const lpAmount = await arthurPair.balanceOf(owner.address);
+            await arthurPair.approve(arthurRouter.address, lpAmount);
+            await arthurRouter.removeLiquidity(tokenA.address, tokenB.address, lpAmount, 0, 0, owner.address, currentTimestamp + ONE_DAY);
+        });
+
+        it('check start time', async () => {
+            let currentTimestamp = await getCurrentTimestamp();
+            await arthurFactory.createPair(tokenA.address, tokenB.address, ONE_DAY, currentTimestamp + ONE_DAY);
+            await arthurRouter.addLiquidity(tokenA.address, tokenB.address, ETHER_100K, ETHER_100K.div(2), ETHER_100K, ETHER_100K.div(2), owner.address, currentTimestamp + ONE_DAY, ONE_DAY, currentTimestamp + ONE_DAY);
+            await expect(arthurRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(ONE_ETHER, 0, [tokenA.address, tokenB.address], owner.address, ethers.constants.AddressZero, currentTimestamp + ONE_DAY)).to.be.revertedWith("INVALID_TIME");
+            
+            await skipTime(ONE_DAY + 1);
+            currentTimestamp = await getCurrentTimestamp();
+            await arthurRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(ONE_ETHER, 0, [tokenA.address, tokenB.address], owner.address, ethers.constants.AddressZero, currentTimestamp + ONE_DAY);
         });
     });
 });
